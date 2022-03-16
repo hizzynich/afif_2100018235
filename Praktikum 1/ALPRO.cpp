991fba06a141e117a56abe7cfb2bde748352100b
Hizzynich <kece.muffler77@gmail.com>
2022-03-16 14:43:51 +0700

    PROJECT BARU

---
+++ Praktikum 1/ALPRO.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int p,q,r,s,a,b;
+	cout<<"Masukan nilai a :"; cin >> a;
+	cout<<"Masukan nilai b :"; cin >> b;
+	p=a+b;
+	q=a-b;
+	r=a*b;
+	s=a/b;
+	cout<<"Nilai a+b :"<<p;
+	cout<<"\n Nilai a-b :"<<q;
+	cout<<"\n nilai a*b :"<<r;
+	cout<<"\n Nilai a/b :"<<s;
+}
